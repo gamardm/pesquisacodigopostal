3ef98dd96c44036c125d38a3fb57595eecba7de0
Gama <root@Mauro-Gama.local>
2015-04-23 22:54:55 +0100

    refactor & count

--- src/lib/PesquisaCodigoPostalAPI.php
+++ src/lib/PesquisaCodigoPostalAPI.php
@@ -23,55 +23,73 @@ class PesquisaCodigoPostalAPI {
 		return var_dump(\Config::get('api-codigopostal-laravel::token'));
 	}
 
-	public function codigoPostalExiste($cp4, $cp3){
-
-		$url 		= $this->url.'/v1/cp/exists/'.$cp4.'/'.$cp3;
-
+	private function call($url){
 
 		$page_data 	= $this->apiConnect->get($url,$this->options);
 		$response 	= $page_data->json();
 
 		return $response;
+	}
+
+	public function codigoPostalExiste($cp4, $cp3){
+
+		$url 		= $this->url.'/v1/cp/exists/'.$cp4.'/'.$cp3;
+
+		return $this->call($url);
 	}
 
 	public function codigoPostal($cp4,$cp3,$offset){
 
 		$url 		= $this->url.'/v1/cp/'.$cp4.'/'.$cp3.'?offset='.$offset;
 
-		$page_data 	= $this->apiConnect->get($url,$this->options);
+		return $this->call($url);
 
-		$response 	= $page_data->json();
-
-		return $response;
 	}
 
 	public function codigoPostal4Digitos($cp4,$offset){
 
 		$url 		= $this->url.'/v1/cp/'.$cp4.'?offset='.$offset;
 
-		$page_data 	= $this->apiConnect->get($url,$this->options);
+		return $this->call($url);
 
-		$response 	= $page_data->json();
-
-		return $response;
 	}
 
-	public function obterCodigoPostalPorMoradaELocalidade($morada,$localidade){
+	public function obterCodigoPostalPorMoradaELocalidade($morada,$localidade, $offset){
 
-		return $morada." -". $localidade;
+		$url 		= $this->url.'/v1/morada/'.$morada.'/localidade/'.$localidade.'?offset='.$offset;
+
+		return $this->call($url);
+
+	}
+
+	public function obterCodigoPostalPorMorada($morada, $offset){
+
+		$url 		= $this->url.'/v1/morada/'.$morada.'?offset='.$offset;
+
+		return $this->call($url);
+
+	}
+	public function countCodigoPostalPorMoradaELocalidade($morada, $localidade){
+
+		$url 		= $this->url.'/v1/count/morada/'.$morada.'/localidade/'.$localidade;
+
+		return $this->call($url);
+
+	}
+
+	public function countCodigoPostalPorMorada($morada){
+		$url 		= $this->url.'/v1/count/morada/'.$morada;
+
+		return $this->call($url);
 	}
 
 	public function countCodigoPostalPrincipal($cp4){
 
 
-
 		$url 		= $this->url.'/v1/cp/count/'.$cp4.'';
 
-		$page_data 	= $this->apiConnect->get($url,$this->options);
+		return $this->call($url);
 
-		$response 	= $page_data->json();
-
-		return $response;
 
 	}
 
@@ -79,12 +97,8 @@ class PesquisaCodigoPostalAPI {
 
 		$url 		= $this->url.'/v1/cp/count/'.$cp4.'/'.$cp3.'';
 
+		return $this->call($url);
 
-		$page_data 	= $this->apiConnect->get($url,$this->options);
-
-		$response 	= $page_data->json();
-
-		return $response;
 
 	}
 
