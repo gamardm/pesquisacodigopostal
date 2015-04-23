05efd7551f3fe51c15b86fa060b5b3bf8c05f6bc
Gama <gama@Mauro-Gama.local>
2015-04-23 22:57:11 +0100

    refactor & count

--- src/lib/PesquisaCodigoPostalAPI.php
+++ src/lib/PesquisaCodigoPostalAPI.php
@@ -12,7 +12,7 @@ class PesquisaCodigoPostalAPI {
 	function __construct()
 	{
 		$this->apiConnect = new Client();
-		$this->url = 'http://codigos-postais';
+		$this->url = 'http://codigos-postais-api.l';
 
 		$this->options = [ 'headers' => [ 'X-Auth-Token' => \Config::get('api-codigopostal-laravel::token')]];
 
